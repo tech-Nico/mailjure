5c9ab25f6088d98593353e1f0a29c7fa975e8d2b
nicoletto <nicoletto@nicoletto-ultra.(none)>
2012-10-03 23:06:11 +0100

    This is the first commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject mailjure "0.1.0-SNAPSHOT"
+            :description "FIXME: write this!"
+            :dependencies [[org.clojure/clojure "1.4.0"]
+                           [com.draines/postal "1.8.0"]
+                           [com.novemberain/monger "1.1.2"]
+                           [enlive "1.0.1"]
+                           [noir "1.3.0-beta3"]]
+
+                           :main mailjure.server)
