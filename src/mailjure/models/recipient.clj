8ad5470d4578d0a112fa870c2ea717266bdf0d5d
nicoletto <nicoletto@nicoletto-ultra.(none)>
2012-11-17 11:49:42 +0000

    Removed recipient.clj

--- src/mailjure/models/recipient.clj
+++
@@ -1,4 +0,0 @@
-(ns mailjure.models.recipient)
-
-(defrecord Address [address1 address2 city region postal-code country])
-(defrecord Recipient [_id email firstname lastname middlename ^Address address])
